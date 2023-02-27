0ba2e4d07aec4dd942c545a8159035db2ef7c3ec
hkgnp <hkgnp@github.com>
2023-02-27 11:43:44 +0800

    [feat] added change font-size, font, line-height

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import "@logseq/libs";
+import { SettingSchemaDesc } from "@logseq/libs/dist/LSPlugin.user";
 import { getDateForPageWithoutBrackets } from "logseq-dateutils";
+
+function provideStyle() {
+  const { fontSize, lineHeight, fontFamily } = logseq.settings!;
+  logseq.provideStyle(`
+											:root {
+											  font-size: ${fontSize.length > 0 ? fontSize : ""}px !important;
+											  line-height: ${lineHeight.length > 0 ? lineHeight : ""} !important;
+												font-family: ${fontFamily.length > 0 ? fontFamily : ""} !important;
+											}
+											`);
+}
 
 function main() {
   console.log("logseq-randomutils-plugin loaded");
+
+  provideStyle();
+  logseq.onSettingsChanged(function () {
+    provideStyle();
+  });
 
   logseq.App.registerCommandPalette(
     {
@@ -40,4 +57,37 @@ function main() {
   );
 }
 
-logseq.ready(main).catch(console.error);
+const settings: SettingSchemaDesc[] = [
+  {
+    key: "heading",
+    type: "heading",
+    default: "",
+    title:
+      "These settings change basic CSS for all of Logseq. For specific customisations, please use a theme or modify custom.css",
+    description: "",
+  },
+  {
+    key: "fontSize",
+    type: "string",
+    default: "",
+    title: "Font Size",
+    description: "Sets the default font-size (in pixels)",
+  },
+  {
+    key: "lineHeight",
+    type: "string",
+    default: "",
+    title: "Line Height",
+    description: "Sets the default line-height",
+  },
+  {
+    key: "fontFamily",
+    type: "string",
+    default: "",
+    title: "Font Family",
+    description:
+      'Sets the default font-family. Use inverted commas if your font has more than 1 word, e.g. "Times New Roman"',
+  },
+];
+
+logseq.useSettingsSchema(settings).ready(main).catch(console.error);
