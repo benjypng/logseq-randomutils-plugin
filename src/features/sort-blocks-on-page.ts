5cd9b489e4c170095755997480fac32027917bd1
hkgnp <ben@pngs.cc>
2023-11-14 12:40:12 +0800

    fix: retain properties when sorting blocks

--- src/features/sort-blocks-on-page.ts
+++ src/features/sort-blocks-on-page.ts
@@ -3,16 +3,21 @@ export const sortBlocksOnPage = () => {
     const pageBlock = await logseq.Editor.getPage(page);
     if (!pageBlock) return;
     const pbt = await logseq.Editor.getPageBlocksTree(page);
-    const sortedPbt = pbt.sort(function (a, b) {
-      var string1 = /[a-zA-Z]+/.exec(a.content);
-      var string2 = /[a-zA-Z]+/.exec(b.content);
+    const pbtWithoutProperties = pbt.filter(
+      (b) => !b.content.startsWith("icon::"),
+    );
+    const sortedPbt = pbtWithoutProperties.sort(function (a, b) {
+      const string1 = /[a-zA-Z]+/.exec(a.content);
+      const string2 = /[a-zA-Z]+/.exec(b.content);
       if (string1 && string2) {
         return string1[0].localeCompare(string2[0]);
       } else {
         return 0;
       }
     });
-    pbt.forEach(async (b) => await logseq.Editor.removeBlock(b.uuid));
+    pbtWithoutProperties.forEach(
+      async (b) => await logseq.Editor.removeBlock(b.uuid),
+    );
     sortedPbt.forEach(
       async (b) =>
         await logseq.Editor.insertBlock(pageBlock?.uuid, b.content, {
